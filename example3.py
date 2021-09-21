48b1305acf33ad4896976a71961c4ccd816167a6
Jon Mockbee <mockbee1000@gmail.com>
2021-09-20 20:26:46 -0400

    special methods

--- example3.py
+++ example3.py
@@ -3,6 +3,9 @@ class Apple:
         self.color = color
         self.flavor = flavor
 
+    def __str__(self):
+        return "This apple is {} and it's flavor is {}".format(self.color, self.flavor)
+
 
 jonagold = Apple("red", "sweet")
-print(jonagold.color)
+print(jonagold)
